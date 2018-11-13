70be382bed285143b85d07b3e80676ad7a7986a4
Ray Zhu <ray.zhu@hpe.com>
2018-11-13 23:24:14 +0800

    add simple route and view

--- web_site/dos_game/views.py
+++ web_site/dos_game/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('index')
+
+
+def game(request, game_id):
+    return HttpResponse('querying %(id)d' % {'id': game_id})
