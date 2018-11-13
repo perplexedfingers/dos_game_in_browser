dc089aa8fb471836a1b1f253c62b72b5928634c1
Ray Zhu <ray.zhu@hpe.com>
2018-11-13 23:23:06 +0800

    Add administration site controls

--- web_site/dos_game/admin.py
+++ web_site/dos_game/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Game, Name, Tag, Language
+
+admin.site.register(Game)
+admin.site.register(Name)
+admin.site.register(Tag)
+admin.site.register(Language)
